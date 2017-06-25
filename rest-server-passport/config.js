0cbded1ce3fe9c113f05516b40b506e5e31ca3aa
liuchang5454 <liuchang5454@gmail.com>
2017-06-24 22:12:32 -0600

    Week4 OAuth and User Authentication.
    Installed passport-facebook module.
    
    Login in through my facebook user through OAuth.

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1711109792526618',
+        clientSecret: '0ad4f552679f8b44eee84ee8a171c8a6',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
--- rest-server-passport/models/user.js
+++ rest-server-passport/models/user.js
@@ -5,17 +5,19 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     username: String,
     password: String,
+    OauthId: String,
+    OauthToken: String,
+    firstname: {
+      type: String,
+      default: ''
+    },
+    lastname: {
+      type: String,
+      default: ''
+    },
     admin:   {
         type: Boolean,
         default: false
-    },
-    firstname:{
-    	type: String,
-    	default: ''
-    },
-    lastname:{
-    	type: String,
-    	default: ''
     }
 });
 
